1fc7020939ee462472c079fdba9fc925d77a8583
yoritin <47884591+yoritin@users.noreply.github.com>
2021-09-25 17:55:01 +0900

    fix _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
