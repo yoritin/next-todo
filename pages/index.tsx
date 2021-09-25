2ba96aac0bf9ee9bbe2b86c0d2fcc02cb6fce03b
yoritin <47884591+yoritin@users.noreply.github.com>
2021-09-25 23:24:25 +0900

    Feature/add chakra UI (#7)
    * add chakra-ui
    
    * fix _app.tsx
    
    * add chakra ui parts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import { FC } from 'react'
 import Layout from '@/components/Layout'
+import { Container, Center, Flex, Input, Box, Text } from '@chakra-ui/react'
 
 const Home: FC = () => {
   return (
     <Layout>
-      <header>Todo App</header>
-      <div>Todo List</div>
+      <Container maxW="container.md">
+        <Center h="100px">
+          <Text fontSize="5xl">Todo List</Text>
+        </Center>
+        <Flex>
+          <Input placeholder="Todo" mr={2} />
+          <Box as="button" borderRadius="md" bg="teal.400" color="white" px={4}>
+            Add
+          </Box>
+        </Flex>
+      </Container>
     </Layout>
   )
 }
